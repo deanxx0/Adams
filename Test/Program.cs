ba564de1cd8b91ecd9183cf8f2c3985376ad01ea
st <deanxx@outlook.kr>
2021-12-28 18:44:02 +0900

    update

--- Test/Program.cs
+++ Test/Program.cs
@@ -8,43 +8,9 @@ Thread.Sleep(3000);
 
 var adams = new AdamsClient("https://localhost:5000", "1", "1");
 
-//for(int i = 0; i < 32; i++)
-//{
-//    var project = new Project(NAVIAITypes.Mercury, $"p{i}", $"p{i}");
-
-//    adams.Projects.Add(project);
-//}
-
-
-//var count = adams.Projects.GetCount();
-
-//var containsTest = adams.Projects.Contains(project1);
-
-//var removeTest = adams.Projects.Remove(project1);
-
-//var containsTest2 = adams.Projects.Contains(project1);
-
-//adams.Projects.Clear();
-
-//var projectGet = adams.Projects.GetProject(project1);
-
-//var projectPage = adams.Projects.GetProjectPage(1).ToList();
-
-////var projectService = new ProjectService(adams, projectGet);
-//var projectService = Extensions.GetService(projectPage[0], adams);
-
-
-
-//var aug = new Augmentataion($"tag{i}");
-
-
-//var count = projectService.Items.Count();
-//var list = projectService.Items.Find(x => true);
-//var page = projectService.Items.Find(x => true, 2, 10);
-
-
-
-
-
+//var project = new Project(NAVIAITypes.Mercury, "p1", "p1");
+//adams.Projects.Add(project);
+var projects = adams.Projects.GetProjectPage(1, 10).ToList();
+var projectService = Extensions.GetService(projects[0], adams);
 
 Console.WriteLine();
